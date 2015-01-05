b3d9368b44338570970a3f918677adbd8ffe4347
Michael D'Auria <michael.dauria@gmail.com>
2015-01-05 17:10:22 -0500

    some progress

---
+++ attributes/datahub.rb
@@ -0,0 +1,15 @@
+default['le']['datahub']['version'] = '1.2.0.261'
+default['le']['datahub']['checksum'] = '69da5a06d83ad60a49fef3d561f6f781cbb3bc55'
+
+default['le']['datahub']['interval'] = 5
+default['le']['datahub']['autoupdate'] = true
+default['le']['datahub']['update_datahub'] = false
+
+default['le']['datahub']['local_path'] = '/etc/leproxy'
+default['le']['datahub']['amazon_s3_secret_key'] = ''
+default['le']['datahub']['amazon_s3_account_id'] = ''
+default['le']['datahub']['amazon_s3_bucket'] = ''
+default['le']['datahub']['amazon_s3_encryption_key_file'] = '/etc/leproxy/public_key.txt'
+
+default['le']['datahub']['rules'] = []
+default['le']['datahub']['filters'] = []
---
+++ recipes/datahub.rb
@@ -0,0 +1,47 @@
+#
+# Author:: Joe Heung <joe.heung@logentries.com>
+# Author:: Michael D'Auria <michaeld@crowdtap.com>
+# Cookbook Name:: le_chef
+# Recipe:: datahub
+#
+# Copyright 2014 Logentries, JLizard
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if platform_family?('rhel')
+  raise NotImplementedError # requires more work since there is no rpm
+end
+
+if platform_family?('debian')
+  datahub_package = "DataHub_#{node['le']['datahub']['version']}.deb"
+
+  remote_file "/tmp/#{datahub_package}" do
+    action :create_if_missing
+    source "http://rep.logentries.com/datahub/#{datahub_package}"
+    mode 0644
+    checksum default['le']['datahub']['checksum']
+  end
+
+  dpkg_package "DataHub" do
+    source "/tmp/#{datahub_package}"
+    action :install
+  end
+end
+
+template "#{node['le']['datahub']['local_path']}/leproxy.config" do
+  source 'etc/leproxy/leproxy.config.erb'
+end
+
+service 'leproxy' do
+  supports :stop => true, :start => true, :restart => true
+  action [ :restart ]
+end
